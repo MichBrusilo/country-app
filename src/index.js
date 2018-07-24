a51205ea564c3d29fc5f9581ba31e6f4fdd058eb
Michał Brusiło <mibru@interia.pl>
2018-07-24 15:11:20 +0200

    Add new files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import DevTools from '../src/DevTools';
+import {getCountries} from '.src/actions/action-countries'
 
 render(
-    <h1>Inicjalizacja projektu</h1>,
+    <Provider store={store}>
+    	<div>
+			<h1>Inicjalizacja projektu</h1>
+        	<DevTools />
+        </div>
+    </Provider>,
     document.getElementById('root')
 );
+
+store.dispatch(getCountries('Countries'))
