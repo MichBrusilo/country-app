8126a5d70a681f14cd3847d25d7073b9ba48bfb5
Michał Brusiło <mibru@interia.pl>
2018-07-25 12:29:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 import routes from './routes';
+import DevTools from '../src/DevTools';
+import './country.css';
 
 render(
     <Provider store={store}>
-        <Router history={hashHistory} routes={routes}/>
+    	<div>
+			<Router history={hashHistory} routes={routes}/>
+        	<DevTools />
+        </div>
     </Provider>,
     document.getElementById('root')
 );
