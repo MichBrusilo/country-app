c76057c595683db0dcf6f8ae83d3beefa8f7f40b
Michał Brusiło <mibru@interia.pl>
2018-07-25 13:32:02 +0200

    Fix bug and finish tasks

--- src/presentational/navigation.component.js
+++ src/presentational/navigation.component.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { Link } from 'react-router';
 import 'bootstrap/dist/css/bootstrap.css';
 
+
 const Navigation = props => (
     <div>
-        <nav className="navbar navbar-default">
+        <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
                 <div className="navbar-header">
                     <Link className="navbar-brand" to="/">Państwa.js</Link>
                 </div>
                 <div className="collapse navbar-collapse">
-                    <ul className="nav navbar-nav">
-                        <li><Link to="/countries">Countries</Link></li>
-                        <li><Link to="/continents">Continents</Link></li>
-                        <li><Link to="/contact">Contact</Link></li>
+                    <ul className="navbar-nav">
+                        <li className="nav-item"><Link className="nav-link" to ="/countries">Countries</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to ="/continents">Continents</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to ="/contact">Contact</Link></li>
                     </ul>
                 </div>
             </div>
