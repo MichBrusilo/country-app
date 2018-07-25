2b7105a9ccfc96e137013a2ffb58aa86e48a29db
Michał Brusiło <mibru@interia.pl>
2018-07-25 11:53:55 +0200

    Add files and update routes

--- src/containers/flag-container.component.js
+++ src/containers/flag-container.component.js
@@ -10,20 +10,27 @@ class CountryFlagContainer extends Component {
 
     componentDidMount() {
         this.props.dispatch(getCountries());
+        this.props.dispatch(searchCountries(''));
     }
 
+    search(event) {
+            this.props.dispatch(searchCountries(event.target.value));
+    }
     render() {
         return (
             <div>
+                <div className="search text-center">
+                    <input type="text" onChange={this.search.bind(this)}/>
+                </div>
                 <CountryFlagList countries={this.props.countries} />
             </div>
         )
-    }
 }
 
 const mapStateToProps = function (store) {
     return {
         countries: store.countriesReducer.countries
+        visibleCountries: store.countriesReducer.visibleCountries
     };
 };
 
